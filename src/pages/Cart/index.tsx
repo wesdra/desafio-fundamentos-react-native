3844cd67988fcde25d979fddbe37c6b2a9edb360
Wesdra LIma <wesdra@hotmail.com>
2020-05-29 22:30:31 -0300

    conclusão do desafio

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -22,9 +22,7 @@ import {
   TotalProductsText,
   SubtotalValue,
 } from './styles';
-
 import { useCart } from '../../hooks/cart';
-
 import formatValue from '../../utils/formatValue';
 
 interface Product {
@@ -39,23 +37,28 @@ const Cart: React.FC = () => {
   const { increment, decrement, products } = useCart();
 
   function handleIncrement(id: string): void {
-    // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
-    // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productSubtotal = product.price * product.quantity;
+      return accumulator + productSubtotal;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+      return accumulator + productsQuantity;
+    }, 0);
+    return total;
   }, [products]);
 
   return (
--- src/pages/Cart/styles.ts
+++ src/pages/Cart/styles.ts
@@ -5,19 +5,16 @@ export const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
 `;
-
 export const ProductContainer = styled.View`
   border-radius: 5px;
   margin-top: 60px;
   flex: 1;
   flex-direction: row;
 `;
-
 export const ProductList = styled(FlatList)`
   flex: 1;
   padding: 0 10px;
 `;
-
 export const Product = styled.View`
   background: #fff;
   padding: 15px 10px;
@@ -26,44 +23,35 @@ export const Product = styled.View`
   flex: 1;
   flex-direction: row;
 `;
-
 export const ProductImage = styled.Image`
   height: 92px;
   width: 92px;
 `;
-
 export const ProductTitleContainer = styled.View`
   font-size: 16px;
   margin-left: 5px;
 `;
-
 export const ProductTitle = styled.Text`
   font-size: 16px;
 `;
-
 export const ProductPriceContainer = styled.View`
   flex-direction: column;
 `;
-
 export const TotalContainer = styled.View`
   flex-direction: row;
   margin-top: 8px;
 `;
-
 export const ProductSinglePrice = styled.Text`
   font-size: 12px;
   color: #a0a0b3;
   margin-top: 8px;
 `;
-
 export const ProductPrice = styled.Text`
   font-weight: bold;
   margin-top: 5px;
-
   font-size: 16px;
   color: #e83f5b;
 `;
-
 export const ProductQuantity = styled.Text`
   font-weight: bold;
   margin-top: 5px;
